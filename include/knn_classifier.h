b286fe3524f8b5937a7f752b46f5f0a10ed3c929
9santa <wotegorb@mail.ru>
2025-10-15 20:05:53 +0300

    header file for k-nearest neighbor classifier

---
+++ include/knn_classifier.h
@@ -0,0 +1,30 @@
+#ifndef KNN_CLASSIFIER_H
+#define KNN_CLASSIFIER_H
+
+#include <vector>
+// #include <algorithm>
+// #include <cmath>
+// #include <map>
+
+struct TrainingSample {
+    std::vector<float> features;
+    int label;
+};
+
+class KNNClassifier {
+public:
+    KNNClassifier(int k = 3);
+    void train(const std::vector<TrainingSample>& trainingData);
+    int predict(const std::vector<float>& features) const;
+    float evaluate(const std::vector<TrainingSample>& testData) const;
+
+private:
+    int k;
+    std::vector<TrainingSample> trainingData;
+
+    float euclideanDistance(const std::vector<float>& a, const std::vector<float>& b) const;
+    
+    std::vector<std::pair<int, float>> findKNearest(const std::vector<float>& features) const;
+};
+
+#endif // KNN_CLASSIFIER_H
