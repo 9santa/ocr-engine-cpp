f5225834cd84f95dbab2dfb02dcdf364c2cb45de
9santa <wotegorb@mail.ru>
2025-10-15 14:29:26 +0300

    finally get opencv to work

--- include/preprocessor.h
+++ include/preprocessor.h
@@ -1,11 +1,24 @@
 #ifndef PREPROCESSOR_H
 #define PREPROCESSOR_H
 
-#include
+#include <opencv4/opencv2/opencv.hpp>
+#include <vector>
+
+class DigitPreprocessor {
+public:
+    DigitPreprocessor();
+    cv::Mat preprocess(const cv::Mat& input);
+    std::vector<cv::Mat> extractDigits(const cv::Mat& image);
+
+private:
+    cv::Mat applyGrayscale(const cv::Mat& input);
+    cv::Mat applyThreshold(const cv::Mat& input);
+    cv::Mat removeNoise(const cv::Mat& input);
+
+};
 
 
 
 
 
-
-#endif // PREPROCESSOR_H}
+#endif // PREPROCESSOR_H
---
+++ src/test.cpp
@@ -0,0 +1,11 @@
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+
+int main() {
+
+
+    std::cout << "OpenCV version : " << CV_VERSION << std::endl;
+    return 0;
+}
+
