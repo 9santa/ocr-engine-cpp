05d503615c7674edcddeb3fa9eae591b69cdbe4c
9santa <wotegorb@mail.ru>
2025-11-04 16:47:43 +0300

    add new evaluation methods

--- include/digit_ocr.h
+++ include/digit_ocr.h
@@ -1,9 +1,12 @@
+#pragma once
 #ifndef DIGIT_OCR_H
 #define DIGIT_OCR_H
 
 #include "feature_extractor.h"
 #include "knn_classifier.h"
 #include "image_matrix.h"
+#include "preprocessor.h"
+#include "mnist_loader.h"
 #include <vector>
 #include <string>
 
@@ -16,13 +19,21 @@ public:
     void saveModel(const std::string& filename);
     void loadModel(const std::string& filename);
 
+    // Testing (evaluation) methods
+    float evaluateOnTestData(const std::string& testDataPath);
+    float evaluateOnTrainingData();
+    void confusionMatrix(const std::vector<MNISTImage>& testData);
+
+    // Check if model is trained
+    bool isTrained() const { return !trainingSamples.empty(); }
+
 private:
-    FeatureExtractor FeatureExtractor;
+    Preprocessor preprocessor;
+    FeatureExtractor featureExtractor;
     KNNClassifier classifier;
     std::vector<TrainingSample> trainingSamples;
 
     std::vector<TrainingSample> loadMNISTData(const std::string& imagePath, const std::string& labelPath);
-
 };
 
 
