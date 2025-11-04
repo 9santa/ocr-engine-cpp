7adb50423a837e51224aa5a73d7ed2692e00e8e3
9santa <wotegorb@mail.ru>
2025-11-04 14:25:52 +0300

    add utility methods for image matrix class

--- src/image_matrix.cpp
+++ src/image_matrix.cpp
@@ -17,3 +17,33 @@ inline unsigned char& ImageMatrix::operator()(int y, int x, int c) {
 inline const unsigned char& ImageMatrix::operator()(int y, int x, int c) const {
     return data[(y * width + x) * channels + c];
 }
+
+bool ImageMatrix::empty() const {
+    return data.empty();
+}
+
+void ImageMatrix::fill(unsigned char value) {
+    std::fill(data.begin(), data.end(), value);
+}
+
+void ImageMatrix::resize(int new_width, int new_height, int new_channels) {
+    // new resized data vector
+    std::vector<unsigned char> new_data(new_width * new_height * new_channels, 0);
+
+    int copy_width = std::min(new_width, width);
+    int copy_height = std::min(new_height, height);
+    int copy_channles = std::min(new_channels, channels);
+
+    for (int y = 0; y < copy_height; y++) {
+        for (int x = 0; x < copy_width; x++) {
+            for (int c = 0; c < copy_channles; c++) {
+                new_data[(y * new_width + x) * new_channels + c] = data[(y * width + x) * channels + c];
+            }
+        }
+    }
+
+    width = new_width;
+    height = new_height;
+    channels = new_channels;
+    data = std::move(new_data);
+}
