4039b8efe938352053f81496ef2c1e6c0f9af59b
9santa <wotegorb@mail.ru>
2025-11-09 16:24:02 +0300

    add algorithm/model types

--- include/digit_ocr.h
+++ include/digit_ocr.h
@@ -2,6 +2,7 @@
 #ifndef DIGIT_OCR_H
 #define DIGIT_OCR_H
 
+#include "neural_network_classifier.h"
 #include "feature_extractor.h"
 #include "knn_classifier.h"
 #include "image_matrix.h"
@@ -10,27 +11,33 @@
 #include <vector>
 #include <string>
 
+enum AlgorithmType {
+    KNN,
+    NEURAL_NETWORK
+};
+
 class DigitOCR {
 public:
     DigitOCR();
 
-    void trainModel(const std::string& trainingDataPath);
-    std::string recognize(const ImageMatrix& image);
-    void saveModel(const std::string& filename);
-    void loadModel(const std::string& filename);
+    void trainModel(const std::string& trainingDataPath, AlgorithmType algo = AlgorithmType::KNN);
+    std::string recognize(const ImageMatrix& image, AlgorithmType algo = AlgorithmType::KNN);
+    void saveModel(const std::string& filename, AlgorithmType algo = AlgorithmType::KNN);
+    void loadModel(const std::string& filename, AlgorithmType algo = AlgorithmType::KNN);
 
     // Testing (evaluation) methods
-    float evaluateOnTestData(const std::string& testDataPath);
+    float evaluateOnTestData(const std::string& testDataPath, AlgorithmType algo = AlgorithmType::KNN);
     float evaluateOnTrainingData();
-    void confusionMatrix(const std::vector<MNISTImage>& testData);
+    void confusionMatrix(const std::vector<MNISTImage>& testData, AlgorithmType algo = AlgorithmType::KNN);
 
     // Check if model is trained
-    bool isTrained() const { return !trainingSamples.empty(); }
+    bool isTrained(AlgorithmType algo = AlgorithmType::KNN) const { return !trainingSamples.empty(); }
 
 private:
     Preprocessor preprocessor;
     FeatureExtractor featureExtractor;
     KNNClassifier classifier;
+    NeuralNetwork nnClassifier;
     std::vector<TrainingSample> trainingSamples;
 
     std::vector<TrainingSample> loadMNISTData(const std::string& imagePath, const std::string& labelPath);
