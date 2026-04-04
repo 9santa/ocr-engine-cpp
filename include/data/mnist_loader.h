cb4ad4ed9417d1830443762140cd08621dcddb7a
9santa <wotegorb@mail.ru>
2026-04-04 16:48:24 +0300

    data loading

---
+++ include/data/mnist_loader.h
@@ -0,0 +1,32 @@
+#pragma once
+#ifndef MNIST_LOADER_H
+#define MNIST_LOADER_H
+
+#include "core/image_matrix.h"
+#include <string>
+#include <vector>
+
+struct MNISTImage {
+    ImageMatrix image;
+    int label;
+};
+
+class MNISTLoader {
+public:
+    MNISTLoader();
+    bool loadTrainingData(const std::string& imagePath, const std::string& labelPath);
+    bool loadTestData(const std::string& imagePath, const std::string& labelPath);
+    const std::vector<MNISTImage>& getTrainingData() const;
+    const std::vector<MNISTImage>& getTestData() const;
+
+private:
+    std::vector<MNISTImage> trainingData;
+    std::vector<MNISTImage> testData;
+
+    int reverseInt(int i);
+    bool loadImages(const std::string& imagePath, std::vector<MNISTImage>& data);
+    bool loadLabels(const std::string& labelPath, std::vector<MNISTImage>& data);
+};
+
+
+#endif // !MNIST_LOADER_H
